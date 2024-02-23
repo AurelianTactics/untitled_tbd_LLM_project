b40d10307fd556655479a83c836e74b1b713e95e
AurelianTactics <jamesdilorenzo@gmail.com>
2024-02-23 06:40:09 -0500

    reorganizing notes

---
+++ utils/prototype_rag_utils.py
@@ -0,0 +1,10 @@
+'''
+Prototype Retrieval Augmented Generation (RAG) utilities
+
+For now simple retriever in prototype_embedding_utils.py works
+
+Can expand to a more advanced RAG system later
+
+Wishlist
+Research methods and ablate the different ways
+'''
